e4fc16d536fcc5f3835f0fb3219edb71baa7cf6b
Orlando <lopezorlando23@gmail.com>
2021-05-03 10:10:12 -0600

    Init

---
+++ PaintingBusinessWebPage/PaintingBusinessWebPage/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PaintingBusinessWebPage.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
